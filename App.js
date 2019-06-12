346440631f070d1b9a4637eaf2d25ad5e83087f4
Raphael <dev@hawks.com.br>
2019-06-12 14:06:16 -0300

    Versão 1 - Lista Rap

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Header from './src/components/Header';
+
+export default function App() {
+  return (
+    <View>
+      <Header />
+    </View>
+  );
+}
+
